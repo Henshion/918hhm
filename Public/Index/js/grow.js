070aa1a963f02b6c775d4f2fce185054bd55d154
Hhm-小明 <Henshion@github.com>
2017-12-15 11:01:44 +0800

    Revert "Initial commit"
    This reverts commit 5b4364770b15163b1a57b031f445de32d3760a3e.

--- Public/Index/js/grow.js
+++
@@ -1,9 +0,0 @@
-;(function(window,document,$){
-	'use strict'
-	var $growLi = $('.container-item','.bd-grow').children();
-	$growLi.hover(function(){
-		$(this).addClass('active');
-	},function(){
-		$(this).removeClass('active');
-	});
-})($(window),$(document),jQuery);
